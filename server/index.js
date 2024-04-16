dc0b580f88b8489e8c06f86c3ddcb14dbcbe456e
revanth332 <revathipathilanka347@gmail.com>
2024-04-16 22:04:10 +0530

    deployment configuration

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["http://localhost:3000",process.env.FRONTEND_URL],
 })
 )
 app.use(cors());
